454531e4f6bff6229943d3ffee056a7bb7510781
Poojitha Naramoni <poojanaramoni054@gmail.com>
2025-10-08 10:35:14 +0530

    Add files via upload

---
+++ Samplename.java
@@ -0,0 +1,10 @@
+public class Samplename
+{
+    public static void main(String[] args) 
+    {
+        //Declare a variable to hold the name
+        String name = "Poojitha";
+        // Print the name to the console
+        System.out.println ("Hello, My name is " + name);
+    }
+}
